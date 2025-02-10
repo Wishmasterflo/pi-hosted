42697b6372867706f9a26c00684324d80e394bfb
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2025-02-10 23:37:21 +0100

    updated path
    updated path

--- tools/install_pixel-server.sh
+++ tools/install_pixel-server.sh
@@ -9,9 +9,9 @@ echo "Enabling spi and i2c communication"
 sudo raspi-config nonint do_spi 0
 sudo raspi-config nonint do_i2c 0
 echo "Creating directory..."
-sudo mkdir /portainer/Files/AppData/Config/pixel-server/
+sudo mkdir /portainer/Config/pixel-server/
 echo "Downloading config files and configuring partly"
-sudo touch /portainer/Files/AppData/Config/pixel-server/users.cfg 
-sudo sh -c "echo 'network_allow_auth = 0.0.0.0' > /portainer/Files/AppData/Config/pixel-server/auth.cfg"
-sudo wget https://raw.githubusercontent.com/penguintutor/pixel-server/main/defaults.cfg -O /portainer/Files/AppData/Config/pixel-server/pixelserver.cfg
+sudo touch /portainer/Config/pixel-server/users.cfg 
+sudo sh -c "echo 'network_allow_auth = 0.0.0.0' > /portainer/Config/pixel-server/auth.cfg"
+sudo wget https://raw.githubusercontent.com/penguintutor/pixel-server/main/defaults.cfg -O /portainer/Config/pixel-server/pixelserver.cfg
 echo "Done You are ready to goto next step in the install document"
