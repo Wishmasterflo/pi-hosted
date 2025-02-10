1ddef7b3df2b7803dbe9e12d2d6187fcbe4a0999
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2025-02-10 23:20:14 +0100

    updated path

--- tools/install_bind9.sh
+++ tools/install_bind9.sh
@@ -18,19 +18,19 @@ function check_internet() {
 check_internet
 
 echo "Creating directories..."
-sudo mkdir -p /portainer/Files/AppData/Config/bind9/ || error "Failed to create config directory!"
-sudo mkdir -p /portainer/Files/AppData/Config/bind9/cache/ || error "Failed to create data directory for Cache!"
-sudo mkdir -p /portainer/Files/AppData/Config/bind9/bind/ || error "Failed to create data directory for Bind"
+sudo mkdir -p /portainer/Config/bind9/ || error "Failed to create config directory!"
+sudo mkdir -p /portainer/Config/bind9/cache/ || error "Failed to create data directory for Cache!"
+sudo mkdir -p /portainer/Config/bind9/bind/ || error "Failed to create data directory for Bind"
 echo "Downloading Prometheus config files if they don't exist"
-if [ -d /portainer/Files/AppData/Config/bind9/named.conf ];
+if [ -d /portainer/Config/bind9/named.conf ];
 then
-    echo "/portainer/Files/AppData/Config/bind9/named.conf is a directory removing"
-    rm -rf '/portainer/Files/AppData/Config/bind9/named.conf'
+    echo "/portainer/Config/bind9/named.conf is a directory removing"
+    rm -rf '/portainer/Config/bind9/named.conf'
 fi
 
-if [ ! -f /portainer/Files/AppData/Config/bind9/named.conf ]; 
+if [ ! -f /portainer/Config/bind9/named.conf ]; 
 then
-	sudo wget -O /portainer/Files/AppData/Config/bind9/named.conf https://raw.githubusercontent.com/pi-hosted/pi-hosted/master/configs/bind9/named.conf || error "Failed to download named.conf file!"
+	sudo wget -O /portainer/Config/bind9/named.conf https://raw.githubusercontent.com/pi-hosted/pi-hosted/master/configs/bind9/named.conf || error "Failed to download named.conf file!"
 fi
 
 echo "Done You are ready to goto next step in the install document"
