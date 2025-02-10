b4548704a8a97a861bcc1bfd5414896e2ee3b683
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2025-02-10 23:26:15 +0100

    updated path
    updated path

--- tools/install_flatnotes.sh
+++ tools/install_flatnotes.sh
@@ -6,7 +6,7 @@ function error {
 }
 
 echo "Creating directory..."
-sudo mkdir -p /portainer/Files/AppData/Config/flatnotes || error "Failed to create folder!"
-sudo mkdir -p /portainer/Files/AppData/Config/flatnotes/data || error "Failed to create folder!"
-sudo chown -R 1000:1000 /portainer/Files/AppData/Config/flatnotes || error "Failed to create folder!"
+sudo mkdir -p /portainer/Config/flatnotes || error "Failed to create folder!"
+sudo mkdir -p /portainer/Config/flatnotes/data || error "Failed to create folder!"
+sudo chown -R 1000:1000 /portainer/Config/flatnotes || error "Failed to create folder!"
 echo "Setup complete. You can now install Flatnotes using the App Template."
