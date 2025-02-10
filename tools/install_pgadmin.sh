9100a07534d77f1be151aaf8663875aa784d96a1
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2025-02-10 23:35:26 +0100

    updated path
    updated path

--- tools/install_pgadmin.sh
+++ tools/install_pgadmin.sh
@@ -6,7 +6,7 @@ function error {
 }
 
 echo "Creating directory..."
-sudo mkdir -p /portainer/Files/AppData/Config/pgadmin || error "Failed to create pgadmin folder!"
-sudo mkdir -p /portainer/Files/AppData/Config/pgadmin || error "Failed to create pgadmin folder!"
-sudo chown -R 5050:root /portainer/Files/AppData/Config/pgadmin || error "Failed to create pgadmin folder!"
+sudo mkdir -p /portainer/Config/pgadmin || error "Failed to create pgadmin folder!"
+sudo mkdir -p /portainer/Config/pgadmin || error "Failed to create pgadmin folder!"
+sudo chown -R 5050:root /portainer/Config/pgadmin || error "Failed to create pgadmin folder!"
 echo "Setup complete. You can now install the PgAdmin using the App Template."
