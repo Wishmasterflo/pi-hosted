6a497af1b5815a5b67272ba644ba0f3bc809fd19
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2025-02-10 23:28:04 +0100

    updated path
    updated path

--- tools/install_homer.sh
+++ tools/install_homer.sh
@@ -6,7 +6,7 @@ function error {
 }
 
 echo "Creating directory..."
-sudo mkdir -p /portainer/Files/AppData/Config/Homer || error "Failed to create Homer folder!"
-sudo mkdir -p /portainer/Files/AppData/Config/Homer/assets || error "Failed to create Homer folder!"
-sudo chown -R 1000:1000 /portainer/Files/AppData/Config/Homer || error "Failed to create Homer folder!"
+sudo mkdir -p /portainer/Config/Homer || error "Failed to create Homer folder!"
+sudo mkdir -p /portainer/Config/Homer/assets || error "Failed to create Homer folder!"
+sudo chown -R 1000:1000 /portainer/Config/Homer || error "Failed to create Homer folder!"
 echo "Setup complete. You can now install the Homer using the App Template."
