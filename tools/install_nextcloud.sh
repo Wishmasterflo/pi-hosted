7ded49c81f23bc98d30c36a647ae6b4a1ec7ecd9
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2025-02-16 15:05:56 +0100

    added DB folder

--- tools/install_nextcloud.sh
+++ tools/install_nextcloud.sh
@@ -7,6 +7,7 @@ function error {
 
 echo "Creating directory..."
 sudo mkdir -p /portainer/Config/Nextcloud/Config || error "Failed to create Config folder!"
-sudo mkdir -p /portainer/Config/Nextcloud/Data || error "Failed to Data folder!"
+sudo mkdir -p /portainer/Config/Nextcloud/Data || error "Failed to create Data folder!"
+sudo mkdir -p /portainer/Config/Nextcloud/DB || error "Failed to create DB folder!"
 sudo chown -R 1000:1000 /portainer/Config/Nextcloud || error "Failed set permission Nextloud folder!"
 echo "Setup complete. You can now install Nextcloud using the App Template. This script specified for Nextcloud stack"
