6dc4dbee5c172c8860e5eff75be52611df7c2403
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2025-04-18 19:19:32 +0200

    Update install_openziti.sh

--- tools/install_openziti.sh
+++ tools/install_openziti.sh
@@ -5,6 +5,16 @@ function error {
   exit 1
 }
 
+function check_internet() {
+  printf "Checking if you are online..."
+  wget -q --spider http://github.com
+  if [ $? -eq 0 ]; then
+    echo "Online. Continuing."
+  else
+    error "Offline. Go connect to the internet then run the script again."
+  fi
+}
+
 echo "Creating directories..."
 sudo mkdir -p /portainer/Config/openziti || error "Failed to create certs directory!"
 
