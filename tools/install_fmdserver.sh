8eaf2107a77c686c7cf0020a1cc46e04185010c9
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2025-12-23 00:01:13 +0100

    Added fmdserver config
    Added fmdserver config

---
+++ tools/install_fmdserver.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+echo "Creating directories..."
+sudo mkdir -p /portainer/Config/fmdserver || error "Failed to create certs directory!"
+sudo mkdir -p /portainer/Config/fmdserver/db || error "Failed to create media directory!"
+sudo mkdir -p /portainer/Config/fmdserver/web || error "Failed to create media directory!"
+
+echo "Setting permissions..."
+sudo chown -R 1000:1000 /portainer/Config/fmdserver || error "Failed to set permissions for Authentik certs!"
+sudo chown -R 1000:1000 /portainer/Config/fmdserver/db || error "Failed to set permissions for Authentik media!"
+sudo chown -R 1000:1000 /portainer/Config/fmdserver/web || error "Failed to set permissions for Authentik geoip!"
+echo " "
+echo "Copying fmsserver Config file..."
+cp /pi-hosted/tools/fmdserver_config.yml /portainer/Config/fmdserver/config.yml
+echo "Done!"
+
+
+
